9cdfa9d1c0bf5933df658244d131a5e460a7dd64
Gustavin <gustavobra247@outlook.com>
2023-03-06 10:42:23 -0300

    criação dos exemplos 06 e 07

---
+++ aula-03/exemplo05/logicos.js
@@ -0,0 +1,27 @@
+// && - and - E
+console.log("operador E ")
+console.log("V E V = " + (true && true))
+console.log("V E V = " + (true && false))
+console.log("V E V = " + (false && true))
+console.log("F E F = " + (false && false))
+/*
+Regra do E - somente terei resposta verdadeira se as duas proposições forem verdadeiras
+*/
+
+// || - or - OU
+console.log("operador OU ")
+console.log("V OU V = " + (true || true))
+console.log("V OU V = " + (true || false))
+console.log("V OU V = " + (false || true))
+console.log("F OU F = " + (false || false))
+/*
+Regra do OU - somente terei resposta verdadeira se as duas proposições forem verdadeiras
+*/
+
+// ! - not - NEGAÇÃO
+console.log("operador E ")
+console.log("!V = " + (!true))
+console.log("!F = " + (!false))
+/*
+Regra do E - somente terei resposta verdadeira se as duas proposições forem verdadeiras
+*/
